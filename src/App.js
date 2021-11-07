69d458b0da28e98b91f844bfdaf550b34cbdae93
unknown <deanshak9@gmail.com>
2021-11-08 00:54:20 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import * as Web3 from "web3";
+import { OpenSeaPort, Network } from "opensea-js";
+import { getDefaultProvider } from "ethers";
+import { NftProvider, useNft } from "use-nft";
+
+import "./App.css";
+// This example provider won't let you make transactions, only read-only calls:
+const provider = new Web3.providers.HttpProvider("https://mainnet.infura.io");
+
+const seaport = new OpenSeaPort(provider, {
+  networkName: Network.Main,
+});
 
 function App() {
+  const ethersConfig = {
+    provider: getDefaultProvider("homestead"),
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {" "}
+      <NftProvider fetcher={["ethers", ethersConfig]}>
+        <Nft />
+      </NftProvider>
     </div>
+  );
+}
+
+function Nft() {
+  const { loading, error, nft } = useNft(
+    "0xf3114dd5c5b50a573e66596563d15a630ed359b4",
+    "668"
+  );
+
+  // nft.loading is true during load.
+  // TODO: Add Loading Spinner
+  if (loading) return <>Loading…</>;
+
+  // nft.error is an Error instance in case of error.
+  if (error || !nft) return <>Error.</>;
+
+  // You can now display the NFT metadata.
+  return (
+    <section>
+      <h1>{nft.name}</h1>
+      <img src={nft.image} alt="" />
+      <p>{nft.description}</p>
+      <p>Owner: {nft.owner}</p>
+      <p>Metadata URL: {nft.metadataUrl}</p>
+    </section>
   );
 }
 
