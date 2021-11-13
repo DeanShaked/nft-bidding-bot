513da603bcb70e57a0c345b8f2907492113ac4e3
unknown <deanshak9@gmail.com>
2021-11-14 00:34:02 +0200

    getAssets First stage

--- src/server/controllers/assets.js
+++ src/server/controllers/assets.js
@@ -1,7 +1,6 @@
 const Web3 = require("web3");
 const Network = require("opensea-js");
 const { OpenSeaPort } = require("opensea-js");
-let assetsList = {};
 
 // This example provider won't let you make transactions, only read-only calls:
 const provider = new Web3.providers.HttpProvider("https://mainnet.infura.io");
@@ -11,17 +10,7 @@ const seaport = new OpenSeaPort(provider, {
 });
 
 const getAssets = async (req, res) => {
-  // const {collectionSlug} = req.params
-
-  fetch(
-    `https://api.opensea.io/api/v1/assets?order_direction=desc&offset=0&collection=divineanarchy`,
-    { method: "GET" }
-  )
-    .then((response) => {
-      assetsList = response.json();
-      console.log(assetsList);
-    })
-    .catch((err) => console.error(err));
+  // const {collectionSlug} = req.params.collectionSlug
 };
 
 module.exports = {
--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -10,8 +10,47 @@ app.get("/", (req, res) => {
   res.json("Working");
 });
 
-app.get("/getAssets", (req, res) => {
-  assets.getAssets(req, res);
+app.get("/getAssets/:collectionSlug/:collectionLength", (req, res) => {
+  // Consts init
+  const collectionLen = req.params.collectionLength;
+  const collectionSlug = req.params.collectionSlug;
+  const requestsLimitAllowed = 30;
+  let limitAllowedMultiple = 1;
+  let promises = [];
+  let i = 2;
+
+  // Iterate through the entire collection,
+  while (i < collectionLen / requestsLimitAllowed) {
+    // Token Ids is used as a query param string containing all the token Id's
+    let tokenIds = "";
+
+    // If the index is equal to to the limit allowed then we'll make a tokenId's bundle
+    if (i === requestsLimitAllowed * limitAllowedMultiple) {
+      // Createa a token id
+      for (let k = 1; k <= 30; k++) {
+        let s = i.toString();
+        tokenIds = tokenIds + `token_ids=${s}&`;
+      }
+
+      limitAllowedMultiple++;
+    }
+    promises.push(
+      fetch(
+        `https://api.opensea.io/api/v1/assets?${tokenIds}&order_direction=desc&limit=${requestsLimitAllowed}&offset=0&collection=${collectionSlug}`,
+        { method: "GET" }
+      )
+    );
+    i++;
+  }
+
+  // Parsing all promises into JSON objects,
+  // Later on, we'll filter to which asset is biddable.
+  Promise.all(promises)
+    .then((values) => {
+      return Promise.all(values.map((r) => r.json())); // Turn each response to JSON format.
+    })
+    .then((values) => res.send(values)) // Send back to the user the assets list
+    .catch((err) => console.error(err));
 });
 
 module.exports = app;
