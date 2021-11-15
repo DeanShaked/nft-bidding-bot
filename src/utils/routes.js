f4e53a6e6fad60069d6fa25c72c69b865b24db8b
unknown <deanshak9@gmail.com>
2021-11-16 01:22:05 +0200

    Working on make offer

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,3 +1,4 @@
 const _URL_GET_ASSETS = `http://localhost:8000/api/getAssets`;
+const _URL_CREATE_BUNDLE_BUY_ORDER = `http://localhost:8000/api/createBundleBuyOrder`;
 
-module.exports = { _URL_GET_ASSETS };
+module.exports = { _URL_GET_ASSETS, _URL_CREATE_BUNDLE_BUY_ORDER };
