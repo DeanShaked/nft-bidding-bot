ac5475feb7aded56b8368150635817affeb37775
unknown <deanshak9@gmail.com>
2021-11-18 19:39:44 +0200

    for roiter

--- src/layout/home/Home.js
+++ src/layout/home/Home.js
@@ -1,41 +1,64 @@
-import axios from "axios";
+// App
 import React from "react";
-import { useSelector } from "react-redux";
-import { getAssets } from "../../api/api";
-import MakeOffer from "../../components/makeOffer/MakeOffer";
-import Search from "../../components/search/Search";
-import { seaport } from "../../utils/config";
 
+// Redux
+import { useSelector } from "react-redux";
+
+// Api
+import { seaport } from "../../utils/config";
+import { getAssets } from "../../api/api";
+import { WyvernSchemaName } from "opensea-js/lib/types";
+
+// Components
+import Search from "../../components/search/Search";
+
+// Styling
 import "./home.scss";
+
 const Home = () => {
+  // Get the collection data from the redux store.
   const collectionSlug = useSelector((state) => state.app.collectionSlug);
   const collectionOffset = useSelector((state) => state.app.collectionOffset);
-  const assetsList = [
-    {
-      tokenId: "",
-      tokenAddress: "",
-    },
-  ];
 
+  // Get the user credentials to make an offer
   const accountAddress = useSelector(
     (state) => state.app.user.metaMaskAccountAddress
   );
 
+  // Initial value for the assets list format.
+  const assetsList = [
+    {
+      tokenId: "",
+      tokenAddress: "",
+      schemaName: WyvernSchemaName,
+    },
+  ];
+
+  // Log the user credentials to make sure
   console.log(accountAddress);
   if (collectionSlug !== "" && collectionOffset !== "") {
     getAssets(collectionSlug, collectionOffset)
       .then((r) => {
         assetsList.push(r);
       })
-      .then(
-        seaport.createBundleBuyOrder({
-          assets: assetsList,
-          accountAddress: accountAddress,
-          startAmount: 0.001,
-          // Optional expiration time for the order, in Unix time (seconds):
-          expirationTime: Math.round(Date.now() / 1000 + 60 * 60 * 24), // One day from now
-        })
-      );
+      .then((r) => {
+        assetsList[1].forEach((asset) => {
+          seaport
+            .createBuyOrder({
+              asset: {
+                tokenAddress: asset.tokenAddress,
+                tokenId: asset.tokenId,
+                schemaName: WyvernSchemaName,
+              },
+              accountAddress: accountAddress,
+              startAmount: 0.001,
+              // Optional expiration time for the order, in Unix time (seconds):
+              expirationTime: Math.round(Date.now() / 1000 + 60 * 60 * 24), // One day from now
+            })
+            .then((r) => console.log(r))
+            .catch((err) => console.error(err));
+        });
+      });
   }
 
   return (
